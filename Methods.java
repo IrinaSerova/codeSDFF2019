3d3c5c6a44eabeb7744837bfef43d40b08e66f34
Irina Serova <isserova@gmail.com>
2018-12-14 12:22:26 -0800

    method range added

--- Methods.java
+++ Methods.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
 
 public class Methods {
-	private static Scanner myNumber;
+	
+	// method 1 The power of two numbers
+	
 	public static void powerOfTwo(){
 		int power = 1;
 		for (int count = 1; count <= 10; count++) {
 			power *= 2;
+			System.out.println("The power of two numbers");
 			System.out.println(power);
 		}		
 	}
+	// method 2 The alarm
+	private static Scanner myNumber;
 	public static void alarm(int num) {
 		myNumber = new Scanner(System.in);
 		System.out.println("Enter the number to be checked.");
@@ -18,11 +23,37 @@ public class Methods {
 			System.out.println("Alarm!");
 		}
 	}
+	// method 3 The sum of integers from 1 to 100
+	public static int sum100() {
+		int sum = 0;
+		for (int count = 1; count <= 100; count++) {
+			sum += count;	
+			
+		}
+		System.out.println("The sum of integers from 1 to 100");
+		System.out.println("The answer is " + sum);
+		return sum;				
+	}
+	// method 4 The sum of the integers in range
+	private static Scanner lowNumber;
+	public static int sumRange(int low, int high) {
+		lowNumber = new Scanner(System.in);
+		System.out.println("Enter the low");
+		low =lowNumber.nextInt();
+		int sum = 0;
+		for (int count = low; count <= high; count++) {
+			sum += count;
+		}
+		System.out.println("The sum of the integers in range");
+		System.out.println("The answer is " + sum);
+		return sum;
+	}
 	
 	public static void main (String args[]) {
 		System.out.println("The first power of two");
 		powerOfTwo();
 		alarm(0);
+		sum100();
 	}
 	
 

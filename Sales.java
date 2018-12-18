03372e0b5d6365b925ec1094999137bf8c36f4bb
Irina Serova <isserova@gmail.com>
2018-12-18 12:03:59 -0800

    Sales added

---
+++ Sales.java
@@ -0,0 +1,78 @@
+// ****************************************************************
+// Sales.java
+//
+// Reads in and stores sales for each of salespeople. 
+// Displays sales entered by salesperson id and total sales for all salespeople,
+// average sale, highest sale, and lowest sale.
+// 
+//
+// **************************************************************** 
+
+import java.util.Scanner;
+import java.text.NumberFormat;
+
+public class Sales
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Enter the number of salespeople: ");
+        int salespeople = scan.nextInt();
+        int[] sales = new int[salespeople];
+        int sum;
+        int maxSalesPerson = 0;
+        int maxSales = 0;
+        int minSalesPerson = 0;
+        int minSales = 0;
+        
+        NumberFormat money = NumberFormat.getCurrencyInstance();
+        
+        for ( int i=0; i<sales.length; i++ )
+        {
+            System.out.print("Enter sales for salesperson " + i + ": ");
+            sales[i] = scan.nextInt();
+            if ( i == 0 )
+            {
+                minSales = sales[i];
+                maxSales = sales[i];
+            }
+            if ( sales[i] > maxSales )
+            {
+                maxSales = sales[i];
+                maxSalesPerson = i;
+            }
+            if ( sales[i] < minSales )
+            {
+                minSales = sales[i];
+                minSalesPerson = i;
+            }
+        }
+        
+        System.out.println("\nSalesperson   Sales");
+        System.out.println("--------------------");
+        sum = 0;
+        for (int i=0; i<sales.length; i++ ){
+            System.out.println("     " + i + "         " + sales[i]);
+            sum += sales[i];
+        }
+        
+        System.out.println("\nTotal sales: " + money.format(sum) );
+        System.out.println("Average sale: " + money.format(sum/salespeople));
+        System.out.println("Salesperson " + maxSalesPerson + " had the highest sale with " + money.format(maxSales) ); 
+        System.out.println("Salesperson " + minSalesPerson + " had the lowest sale with  " + money.format(minSales) );
+        
+        System.out.print("Enter the value to exceed: ");
+        int exceed = scan.nextInt();
+        int exceedCount = 0;
+        for( int i=0; i < sales.length; i++ ){
+            if( sales[i] > exceed )
+            {
+                System.out.println("Salesperson " + i + " Sales = " + money.format(sales[i]) );
+                exceedCount++;
+            }
+        }
+ 
+        System.out.println( exceedCount + " Salespeople exceeded " + money.format(exceed) );
+    }
+}
---
+++ Switch.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+public class Switch {
+
+	public static void main(String args[]) {
+		Scanner inPut = new Scanner(System.in);
+		int inputter = inPut.nextInt();
+		int outPut = 0;
+		switch (inputter) {
+		case 1:
+			outPut+=4;
+			break;
+		case 2:
+			outPut+=8;
+			break;
+		case 3:
+			outPut-=3;
+			break;
+		case 4:
+			outPut-=2;
+			break;
+		case 5:
+			outPut=outPut*5;
+			break;
+		case 6:
+			outPut=outPut%4;
+			break;
+		case 7:
+			outPut=17;
+			break;
+		case 8:
+			outPut++;
+			break;
+		case 9:
+			++outPut;
+			break;
+		case 10:
+			outPut=1;
+			break;
+		default:
+			outPut=0;
+		}
+		
+		System.out.println(outPut);
+		
+	}
+	}
+		//String a = answer(int);
+		//System.out.println(a);
+
+	
+	
+	
+//	public int answer(Class<Integer> class1) {
+//		S
+//}
